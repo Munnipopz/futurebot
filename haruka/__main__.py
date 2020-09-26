459b2876e5411301b4e7562f4ee3948dd8a1f407
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-09-26 21:04:29 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -144,7 +144,7 @@ Follow [Zains Projects](https://t.me/zainsprojects) ( @ZainsProjects ) if you wa
 
     keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/HarukaAyaGroup")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
-    keyboard = [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_")]] 
+    keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
     keyboard += [[InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
 
     update.effective_message.reply_text(text, reply_markup=InlineKeyboardMarkup(keyboard), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
